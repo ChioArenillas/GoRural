06eb40551cd52acc5afe1f7eb648cfe51209762f
Rocio Arenillas (Chio) <arenillasr@gmail.com>
2026-01-19 16:57:48 +0100

    new

--- src/components/Experiences/Experiences.jsx
+++ src/components/Experiences/Experiences.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react'
 import { useRouter } from 'next/router'
 import styles from "./Experiences.module.css";
 import { getExperiences } from '@/pages/api/userFecht';
+import Link from 'next/link';
 
 
 export default function Experiences() {
@@ -16,21 +17,30 @@ export default function Experiences() {
     setExperiences(experiencesAux)
   }, [])
 
-  
+  //AÑADIR EL BUSCADOR Y EL FILTRO POR CATEGORÍA, 
   return (
 
 <div className={styles.experiencesList}>
   {experiences.map((experience) => (
-      <div className={styles.experienceCard} key={experience.id}>
-        <div className={styles.experienceDetailsTop}>
-          <img src={experience.img} alt={experience.title} className={styles.experienceImage}/>
-          <p className={styles.experienceCategorie}>{experience.category}</p>
-          <h3 className={styles.experienceTitle}>{experience.title}</h3>
-          <p className={styles.experienceDuration}>{experience.duration}</p>
-        </div>
-        <div className={styles.experienceDetailsBottom}>
-          <p className={styles.experiencePrice}>{experience.price}{experience.currency}</p>
-          <p className={styles.experienceRating}> ⭐ {experience.rating} ({experience.reviews})</p>
+      <div key={experience.id}>
+        <div className={styles.experienceCard}>
+          <Link className={styles.cardLink} href={{
+            pathname: 'DetailPage',
+            query: {
+              id: experience.id
+            }
+            }}>
+            <div className={styles.experienceDetailsTop}>
+              <img src={experience.img} alt={experience.title} className={styles.experienceImage}/>
+              <p className={styles.experienceCategorie}>{experience.category}</p>
+              <h3 className={styles.experienceTitle}>{experience.title}</h3>
+              <p className={styles.experienceDuration}>{experience.duration}</p>
+            </div>
+            <div className={styles.experienceDetailsBottom}>
+              <p className={styles.experiencePrice}>{experience.price}{experience.currency}</p>
+              <p className={styles.experienceRating}> ⭐ {experience.rating} ({experience.reviews})</p>
+            </div>
+          </Link>
         </div>
       </div>
     ))}
